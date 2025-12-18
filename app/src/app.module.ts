6176e37f969f0fd4b2c817f821ec1873eb0533b1
Hansol Ryu <ryu.axel@gmail.com>
2025-12-18 22:40:02 +0900

    feature: adopt redis

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './common/redis /redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
